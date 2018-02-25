b720aaadf6d067f472ff80632d088365b09a9bd5
damjanh <damjan.hudobreznik@gmail.com>
2018-02-25 10:41:01 +0100

    Ball & Reorganize
     - Add bowling ball.
     - Reorganize structure.
     - Add ball sounds.
     - Add ball meterial.
     - Add ball initial physics.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	private Rigidbody thisRigidbody;
+
+	private AudioSource audioSource;
+
+	public float launchSpeed;
+
+	void Start () {
+		thisRigidbody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
+
+		Launch();
+	}
+
+	public void Launch() {
+		thisRigidbody.velocity = new Vector3(0, 0, launchSpeed);
+		audioSource.Play();
+	}
+}
