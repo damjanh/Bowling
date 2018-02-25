c77350435209f7fe62d9c826a869b6417975bfe9
damjanh <damjan.hudobreznik@gmail.com>
2018-02-25 18:40:59 +0100

    Count standing pins.
     - Implement logic for counting remaining pins.

---
+++ Assets/Scripts/PinSetter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PinSetter : MonoBehaviour {
+
+	private Pin[] pins;
+
+	public Text standingPinsCountText;
+
+	void Start () {
+		pins = FindObjectsOfType<Pin>();
+
+		standingPinsCountText.text = CountStatnding().ToString();
+	}
+
+	int CountStatnding() {
+		int standing = 0;
+
+		foreach(Pin pin in pins) {
+			if (pin.IsStanding()) {
+				standing ++;
+			}
+		}
+		return standing;
+	}
+}
