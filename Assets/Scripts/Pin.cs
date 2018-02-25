d376f18d9ef1dad164350765ad7dc1d7d596c3f8
damjanh <damjan.hudobreznik@gmail.com>
2018-02-25 18:15:05 +0100

    Detect standing pins.
     - Implement logic to detect standing pins.
     - Remove useless BowlingPin prefab.

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+	public float standingThreshold = 3f;
+
+	public bool IsStanding() {
+		float tiltX = Mathf.Abs(transform.rotation.eulerAngles.x);
+		float tiltZ = Mathf.Abs(transform.rotation.eulerAngles.z);
+		
+		return tiltX < standingThreshold && tiltZ < standingThreshold;
+	}
+}
