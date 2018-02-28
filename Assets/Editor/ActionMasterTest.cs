8fe18c2934479068603a948069eb3d1349f17b41
damjanh <damjan.hudobreznik@gmail.com>
2018-02-28 20:41:24 +0100

    Implement ActionMaster & Tests.
     - Implement ActionMaster.
     - Implment tests for ActionMaster.

---
+++ Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+public class ActionMasterTest {
+
+	private ActionMaster.Action endTurn = ActionMaster.Action.END_TURN;
+	private ActionMaster.Action tidy = ActionMaster.Action.TIDY;
+	private ActionMaster.Action reset = ActionMaster.Action.RESET;
+	private ActionMaster.Action endGame = ActionMaster.Action.END_GAME;
+
+	private ActionMaster actionMaster;
+
+	[SetUp]
+	public void Setup() {
+		actionMaster = new ActionMaster();
+	}
+
+	[Test]
+	public void T00_PassingTest () {
+		Assert.AreEqual (1, 1);
+	}
+
+	[Test]
+	public void T01_StrikeReturnsEndTurn() {
+		Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+	}
+
+	[Test]
+	public void T02_Bowl8ReturnsTidy() {
+		Assert.AreEqual(tidy, actionMaster.Bowl(8));
+	}
+
+	[Test]
+	public void T03_Bowl28SpareReturnsEndtTurn() {
+		actionMaster.Bowl(2);
+		Assert.AreEqual(endTurn, actionMaster.Bowl(8));
+	}
+
+	[Test]
+	public void T04_BowlStrikeInLastFrameReturnsReset() {
+		int [] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+		foreach(int roll in rolls) {
+			actionMaster.Bowl(roll);
+		}
+		Assert.AreEqual(reset, actionMaster.Bowl(10));
+	}
+
+	[Test]
+	public void T05_BowlSpareInLasetFrameReturnsReset() {
+		int [] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+		foreach(int roll in rolls) {
+			actionMaster.Bowl(roll);
+		}
+		actionMaster.Bowl(5);
+		Assert.AreEqual(reset, actionMaster.Bowl(5));
+	}
+
+	[Test]
+	public void T06_GameEndSpareLastFrame() {
+		int [] rolls = { 8, 2, 7, 3, 3, 4, 10, 2, 8, 10, 10, 8, 0, 10, 8, 2 };
+		foreach(int roll in rolls) {
+			actionMaster.Bowl(roll);
+		}
+		Assert.AreEqual(endGame, actionMaster.Bowl(9));
+	}
+
+	[Test]
+	public void T07_GameEndsAtBowl20() {
+		int [] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+		foreach(int roll in rolls) {
+			actionMaster.Bowl(roll);
+		}
+		Assert.AreEqual(endGame, actionMaster.Bowl(1));
+	}
+
+	[Test]
+	public void T08_LastFrameBowl10_1ReturnsReset() {
+		int [] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10};
+		foreach(int roll in rolls) {
+			actionMaster.Bowl(roll);
+		}
+		Assert.AreEqual(tidy, actionMaster.Bowl(1));
+	}
+
+	[Test]
+	public void T09_LastFrameBowl10_0ReturnsTidy() {
+		int [] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10};
+		foreach(int roll in rolls) {
+			actionMaster.Bowl(roll);
+		}
+		Assert.AreEqual(tidy, actionMaster.Bowl(0));
+	}
+
+	[Test]
+	public void T10_Bowl0_10_5_1ReturnEndTurn () {
+		int[] rolls = {0, 10, 5};
+		foreach(int roll in rolls) {
+			actionMaster.Bowl(roll);
+		}
+		Assert.AreEqual (endTurn, actionMaster.Bowl(1));
+	}
+
+	[Test]
+	public void T11_LastThreeFramesStrikeReturnsEndGame () {
+		int[] rolls = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 10};
+		foreach(int roll in rolls) {
+			actionMaster.Bowl(roll);
+		}
+		Assert.AreEqual (endGame, actionMaster.Bowl(10));
+	}
+
+	[Test]
+	public void T12_Bowl0_1ReturnsEndTurn () {
+		actionMaster.Bowl(0);
+		Assert.AreEqual (endTurn, actionMaster.Bowl(1));
+	}
+}
---
+++ Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMaster {
+
+	private int[] bowls = new int[21];
+	private int bowlNumber = 1;
+
+	public enum Action { TIDY, RESET, END_TURN, END_GAME };
+
+	public Action Bowl (int pins) {
+		if (pins < 0 || pins > 10) {throw new UnityException ("Invalid pins");}
+
+		bowls [bowlNumber - 1] = pins;
+
+		if (bowlNumber == 21) {
+			return Action.END_GAME;
+		}
+
+		// Llast-frame special cases
+		if (bowlNumber >= 19 && pins == 10) {
+			bowlNumber++;
+			return Action.RESET;
+		} else if (bowlNumber == 20) {
+			bowlNumber++;
+			if (bowls[18] == 10 && bowls[19] == 0) {
+				return Action.TIDY;
+			} else if (bowls[18] + bowls[19] == 10) {
+				return Action.RESET;
+			} else if (Bowl21Awarded()) {
+				return Action.TIDY;
+			} else {
+				return Action.END_GAME;
+			}
+		}
+
+		if (bowlNumber % 2 != 0) {
+			// First frame
+			if (pins == 10) {
+				bowlNumber += 2;
+				return Action.END_TURN;
+			} else {
+				bowlNumber ++;
+				return Action.TIDY;
+			}
+		} else if (bowlNumber % 2 == 0) {
+			// Second frame
+			bowlNumber ++;
+			return Action.END_TURN;
+		}
+
+		throw new UnityException ("Not sure what action to return!");
+	}
+
+	private bool Bowl21Awarded () {
+		return (bowls [18] + bowls [19] >= 10);
+	}
+}
