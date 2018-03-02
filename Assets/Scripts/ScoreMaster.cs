84969f442fb4bf7ac77b3d53b73784e27ecd87d4
damjanh <damjan.hudobreznik@gmail.com>
2018-03-02 20:13:20 +0100

    ScoreMaster and Tests.
     - Implement script for calculating the bowling score.
     - Implement tests for the ScoreMaster.

--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -5,11 +5,41 @@ using UnityEngine;
 public class ScoreMaster  {
 
 	public static List<int> ScoreFrames(List<int> rolls) {
-		List<int> frameList = new List<int>();
+		List<int> frames = new List<int>();
+		for (int i = 1; i < rolls.Count; i += 2) {
+			if (frames.Count == 10) {
+				// Only 11 frames exist.
+				break;
+			}	
 
-		//TODO
+			if (rolls[i - 1] + rolls[i] < 10) {				
+				frames.Add (rolls [i - 1] + rolls [i]);
+			}
 
-		return frameList;
+			if (i >= rolls.Count - 1) {
+				break;
+			}
+
+			if (rolls[i-1] == 10) {							
+				i--; // Strike - one bowl.										
+				frames.Add (10 + rolls [i + 1] + rolls[i + 2]);
+			} else if (rolls[i - 1] + rolls[i] == 10) {	
+				// Spare	
+				frames.Add (10 + rolls [i + 1]);
+			}
+		}
+		return frames;
 	}
 
+	public static List<int> ScoreCumulative(List<int> rolls) {
+		List<int> cumulativeScores = new List<int>();
+		int total = 0;
+
+		foreach(int frameScore in ScoreFrames(rolls)) {
+			total += frameScore;
+			cumulativeScores.Add(total);
+		}
+
+		return cumulativeScores;
+	}
 }
